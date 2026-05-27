f0502df353cdaceaf173cb3452df2c54ff68a981
LucasAraujo1016 <lucas.allbuquerque1234@gmail.com>
2026-05-27 10:41:35 -0300

    feat: #scrum-65 conexão do publisher iot ao EMQX Broker

---
+++ config.example.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID     "sua_rede"
+#define WIFI_PASSWORD "sua_senha"
+
+#define MQTT_SERVER "seu_broker.emqxsl.com"
+#define MQTT_PORT   8883
+#define MQTT_USER   "seu_usuario"
+#define MQTT_PASS   "sua_senha"
+
+#endif
